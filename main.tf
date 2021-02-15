712997e527dabd5e7651d909b00847f0f77b6632
Michael Kennedy <merpso@gmail.com>
2021-02-15 16:36:39 +1100

    initial EKS module commit before walkies

---
+++ main.tf
@@ -0,0 +1,145 @@
+data "aws_availability_zones" "available_azs" {
+  state = "available"
+}
+
+data "aws_caller_identity" "current" {} # used for accesing Account ID and ARN
+
+# render Admin & Developer users list with the structure required by EKS module
+locals {
+  admin_user_map_users = [
+    for admin_user in var.admin_users :
+    {
+      userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${admin_user}"
+      username = admin_user
+      groups   = ["system:masters"]
+    }
+  ]
+  developer_user_map_users = [
+    for developer_user in var.developer_users :
+    {
+      userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${developer_user}"
+      username = developer_user
+      groups   = ["${var.context.prefix}-developers"]
+    }
+  ]
+  worker_groups_launch_template = [
+    {
+      override_instance_types = var.asg_instance_types
+      asg_desired_capacity    = var.autoscaling_minimum_size_by_az * length(data.aws_availability_zones.available_azs.zone_ids)
+      asg_min_size            = var.autoscaling_minimum_size_by_az * length(data.aws_availability_zones.available_azs.zone_ids)
+      asg_max_size            = var.autoscaling_maximum_size_by_az * length(data.aws_availability_zones.available_azs.zone_ids)
+      kubelet_extra_args      = "--node-labels=node.kubernetes.io/lifecycle=spot" # use Spot EC2 instances to save some money and scale more
+      public_ip               = true
+    },
+  ]
+}
+
+# create EKS cluster
+module "eks-cluster" {
+  source           = "terraform-aws-modules/eks/aws"
+  version          = "12.1.0"
+  cluster_name     = var.context.prefix
+  cluster_version  = "1.16"
+  write_kubeconfig = false
+
+  subnets = var.aws_vpc.private_subnets
+  vpc_id  = var.aws_vpc.vpc_id
+
+  worker_groups_launch_template = local.worker_groups_launch_template
+
+  # map developer & admin ARNs as kubernetes Users
+  map_users = concat(local.admin_user_map_users, local.developer_user_map_users)
+}
+
+# get EKS cluster info to configure Kubernetes and Helm providers
+data "aws_eks_cluster" "cluster" {
+  name = module.eks-cluster.cluster_id
+}
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks-cluster.cluster_id
+}
+
+# deploy spot termination handler
+resource "helm_release" "spot_termination_handler" {
+  name       = var.spot_termination_handler_chart_name
+  chart      = var.spot_termination_handler_chart_name
+  repository = var.spot_termination_handler_chart_repo
+  version    = var.spot_termination_handler_chart_version
+  namespace  = var.spot_termination_handler_chart_namespace
+}
+
+# add spot fleet Autoscaling policy
+resource "aws_autoscaling_policy" "eks_autoscaling_policy" {
+  count = length(local.worker_groups_launch_template)
+
+  name                   = "${module.eks-cluster.workers_asg_names[count.index]}-autoscaling-policy"
+  autoscaling_group_name = module.eks-cluster.workers_asg_names[count.index]
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = var.autoscaling_average_cpu
+  }
+}
+
+# create all Namespaces into EKS
+resource "kubernetes_namespace" "eks_namespaces" {
+  for_each = toset(var.namespaces)
+
+  metadata {
+    annotations = {
+      name = each.key
+    }
+    name = each.key
+  }
+}
+
+# create developers Role using RBAC
+resource "kubernetes_cluster_role" "iam_roles_developers" {
+  metadata {
+    name = "${var.context.prefix}-developers"
+  }
+
+  rule {
+    api_groups = ["*"]
+    resources  = ["pods", "pods/log", "deployments", "ingresses", "services"]
+    verbs      = ["get", "list"]
+  }
+
+  rule {
+    api_groups = ["*"]
+    resources  = ["pods/exec"]
+    verbs      = ["create"]
+  }
+
+  rule {
+    api_groups = ["*"]
+    resources  = ["pods/portforward"]
+    verbs      = ["*"]
+  }
+}
+
+# bind developer Users with their Role
+resource "kubernetes_cluster_role_binding" "iam_roles_developers" {
+  metadata {
+    name = "${var.context.prefix}-developers"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "${var.context.prefix}-developers"
+  }
+
+  dynamic "subject" {
+    for_each = toset(var.developer_users)
+
+    content {
+      name      = subject.key
+      kind      = "User"
+      api_group = "rbac.authorization.k8s.io"
+    }
+  }
+}
---
+++ varibles.tf
@@ -0,0 +1,66 @@
+# import aws infra
+variable "aws_vpc" {
+  type = object({
+    cidr            = string
+    azs             = list(string)
+    vpc_id          = string
+    private_subnets = list(string)
+    public_subnets  = list(string)
+    region          = string
+  })
+}
+
+variable "context" {
+  type = object({
+    prefix = string
+    random = string
+  })
+}
+
+# create some variables
+variable "admin_users" {
+  type        = list(string)
+  description = "List of Kubernetes admins."
+}
+variable "developer_users" {
+  type        = list(string)
+  description = "List of Kubernetes developers."
+}
+variable "asg_instance_types" {
+  type        = list(string)
+  description = "List of EC2 instance machine types to be used in EKS."
+}
+variable "autoscaling_minimum_size_by_az" {
+  type        = number
+  description = "Minimum number of EC2 instances to autoscale our EKS cluster on each AZ."
+}
+variable "autoscaling_maximum_size_by_az" {
+  type        = number
+  description = "Maximum number of EC2 instances to autoscale our EKS cluster on each AZ."
+}
+variable "autoscaling_average_cpu" {
+  type        = number
+  description = "Average CPU threshold to autoscale EKS EC2 instances."
+}
+variable "spot_termination_handler_chart_name" {
+  type        = string
+  description = "EKS Spot termination handler Helm chart name."
+}
+variable "spot_termination_handler_chart_repo" {
+  type        = string
+  description = "EKS Spot termination handler Helm repository name."
+}
+variable "spot_termination_handler_chart_version" {
+  type        = string
+  description = "EKS Spot termination handler Helm chart version."
+}
+variable "spot_termination_handler_chart_namespace" {
+  type        = string
+  description = "Kubernetes namespace to deploy EKS Spot termination handler Helm chart."
+}
+
+# create some variables
+variable "namespaces" {
+  type        = list(string)
+  description = "List of namespaces to be created in our EKS Cluster."
+}
